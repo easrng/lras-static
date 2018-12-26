54a5c08ae397569ddb3ccf5f0bee57a1318912a5
easrng <easrng@gmail.com>
2018-12-26 10:31:21 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-(function (lr,as){
-    'use strict';
-    lr.onload=function (){
-        lr.onmessage=function (m){
-            as.querySelector("iframe#info").height=m.data+"px";
-        }.bind(this);
-    }.bind(this);
-})(window,document);
